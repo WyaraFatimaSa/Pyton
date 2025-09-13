c04553edbd0a431036a7589bd2e2b4189a062e1b
Wyara <wyara.f@outlook.com>
2025-09-13 19:06:36 -0300

    Create EstruturasCondicionais.py

---
+++ EstruturasCondicionais.py
@@ -0,0 +1,8 @@
+numero = int(input("Selecione o número da opção desejada ou aperte 0 para finalizar"))
+while numero != 0:
+    if numero % 2 == 0:
+        print ("O numero é par ")
+    else:
+        print ("O numero é impar")
+    numero = int(input (" Digite um numero ou  0 para sair      "    ))
+  
